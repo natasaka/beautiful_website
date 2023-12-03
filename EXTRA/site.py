1c973ca7721de1bcb36aa82027e5ae57fb893c83
natasaka <79475262+natasaka@users.noreply.github.com>
2023-12-03 19:44:24 +0100

    Create site.py

---
+++ EXTRA/site.py
@@ -0,0 +1,16 @@
+from twisted.internet import reactor
+from twisted.web.server import Site
+from twisted.web.resource import Resource
+import subprocess
+
+class WebSite(Resource):
+    isLeaf = True
+    
+    def render_GET(self, request):
+        content = b'''cat greetings.txt | grep "''' + request.args[b'name'][0] + b'''" | awk -F"|" '{printf $2}' '''
+        greeting = subprocess.check_output(content, shell=True)
+        return b'<html><body>' + bytearray(greeting) + b' ' + request.args[b'name'][0] + b'!</body></html>'
+
+listen_site = Site(WebSite())
+reactor.listenTCP(8080, listen_site)
+reactor.run()
